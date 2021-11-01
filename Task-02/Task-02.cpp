1de805e5b996968957957673ffcd176aef23c8ec
aayushvrt44 <91362918+aayushvrt44@users.noreply.github.com>
2021-11-01 09:27:01 +0530

    Add files via upload

---
+++ Task-02/Task-02.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* local variable is same as a member's name */
+class Test
+{
+private:
+	string name;
+	char sec;
+	int rollno;
+	public:
+	void setX (string name,char sec,int rollno )
+	{
+		
+		this->name = name;
+		this->sec = sec;
+		this->rollno = rollno;
+	}
+	void print() 
+	{
+	 cout << "Name,Section,Roll Number: " << name<<" "<<sec<<" "<<rollno<<" " << endl; 
+	 }
+};
+
+int main()
+{
+Test obj;
+int x = 20;
+string n="Aayush kumar";
+char c='C';
+int roll=01;
+obj.setX(n,c,roll);
+obj.print();
+return 0;
+}
